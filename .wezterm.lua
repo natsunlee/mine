3526806c761f9d99ae67f5fd320eeec44085ae1d
Nathan Lee <lee.nathan.sh@outlook.com>
2024-08-18 00:27:05 -0400

    updated font size for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,6 +2,8 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.font_size = 16.0
+
 -- Theme
 config.color_scheme = 'catppuccin-frappe'
 config.font = wezterm.font 'Fira Code'
